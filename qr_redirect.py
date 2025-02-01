0556cdb3785a569162ac8b67d2f555a52acbf6cb
ProdromosKir <81974173+ProdromosKir@users.noreply.github.com>
2025-02-01 16:15:24 +0200

    first demo of the 23_ldb_menu app

---
+++ qr_redirect.py
@@ -0,0 +1,17 @@
+import qrcode
+
+url = "http://localhost:5000"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+qr_image = qr.make_image(fill="black",back_color="white")
+qr_image.save("menu_qr_code.png")
+print("QR Code generated succesfully!")
