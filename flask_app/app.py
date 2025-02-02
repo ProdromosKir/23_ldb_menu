50b4b62505d9b8d4bcfd5d1ceaf5c46ed82e0d0e
ProdromosKir <81974173+ProdromosKir@users.noreply.github.com>
2025-02-02 13:10:13 +0200

    fixed excel connectivity

--- flask_app/app.py
+++ flask_app/app.py
@@ -14,8 +14,7 @@ app = Flask(__name__)
 # Replace this URL with the link to your Google Sheet (CSV format)
 sheet_url = "https://docs.google.com/spreadsheets/d/1gYnWMG9MZigPiUTaWvz5NEC_S8Fb11HGhXmUmvOvG4g/pub?output=csv"
 
-# Read the CSV data from the Google Sheet
-df = pd.read_csv(sheet_url)
+
 
 class FoodItem:
     def __init__(self,food,value,url,category):
@@ -27,18 +26,22 @@ class FoodItem:
         return f"FoodItem(food='{self.food}', value={self.value}, url='{self.url}')"
 
 
-food_items = []
-
-for _, row in df.iterrows():
-    food_item = FoodItem(food = row['Προιόν'], value=row['Τιμή'], url=row['URL'], category=row['Κατηγορία'])
-    food_items.append(food_item)
-
-for item in food_items:
-    print(item.food)
 
 
 @app.route('/')
 def index():
+    # Read the CSV data from the Google Sheet
+    df = pd.read_csv(sheet_url)
+
+    food_items = []
+
+    for _, row in df.iterrows():
+        food_item = FoodItem(food = row['Προιόν'], value=row['Τιμή'], url=row['URL'], category=row['Κατηγορία'])
+        food_items.append(food_item)
+
+    for item in food_items:
+        print(item.food)
+
     return render_template('index.html', food_items=food_items)
 
 if __name__ == "__main__":
